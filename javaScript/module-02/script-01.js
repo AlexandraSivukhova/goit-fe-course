1c835c22d4d7d9f77de50d72254437c5da5d4dfc
AlexandraSivukhova <kolotova9@gmail.com>
2018-09-01 14:43:00 +0300

    Module-02

--- javaScript/module-02/script-01.js
+++ javaScript/module-02/script-01.js
@@ -6,16 +6,14 @@ let total = 0;
 
 do {
   userInput = prompt('Введите любое число');
-    if ( !isNaN(userInput)
-    && parseInt(Number(userInput)) == userInput
-    && !isNaN(parseInt(userInput, 10)) ) {
-    numbers.push(Number(userInput)); 
-    } 
+ if ( !isNaN(userInput)) {
+  numbers.push(+userInput); 
+ }
 } while (userInput !== null);
 
-console.log(numbers);
-
-for (let sum of numbers) {
-  total += sum;
+if (numbers.length > 0) {
+ for (let i of numbers) {
+  total += i;
+  }
+  alert(`Cумма: ${total}`);
 }
-alert(`Cумма: ${total}`);
